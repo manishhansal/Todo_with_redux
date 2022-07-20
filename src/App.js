a1638a48700b103575f0b33d9e5a33ba57b6155e
Manish Kumar <manishhansalmeh@gmail.com>
2022-07-20 17:07:53 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import DisplayTodos from './Components/DisplayTodos';
 import Todo from './Components/Todo';
 
 function App() {
   return (
     <div className="App">
-      <Todo/>
+      <Todo />
+      <DisplayTodos/>
     </div>
   );
 }
